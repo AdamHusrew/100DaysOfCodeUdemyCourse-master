f9ed6abd59830d336c37f003411381dd4a7b845a
AdamHusrew <Adam.Husrew@cydeo.com>
2022-09-18 08:56:28 +0300

    Day 2 Completed

--- day02_understanding_data_types/tip_calculator.py
+++ day02_understanding_data_types/tip_calculator.py
@@ -11,8 +11,8 @@
 print("Welcome to the tip calculator!")
 bill=float(input("What was the total bill? $"))
 tip=int(input("Hoe much tip would you like to give? 10,12, or 15? "))
-people=int(input("How many people to split the bill?" ))
+people=int(input("How many people to split the bill? " ))
 
-bill_with_tip=tip/100*bill+bill
+bill_with_tip=tip/100*bill+bill/people
 
-print(f"each person should pay {bill_with_tip} ")
+print(f"each person should pay ${bill_with_tip} ")
