23f747e820ebd4e7f0cb0742438322a40a149786
Adam Husrew <Adam.Husrew@cydeo.com>
2022-10-02 14:30:53 +0300

    Day 4 Started

---
+++ day04_randomisationAndPyhtonLists/randomisation.py
@@ -0,0 +1,13 @@
+import random
+
+random_int = random.randint(1, 10)
+print(random_int)
+
+random_float = random.random()
+print(random_float)
+
+random_custom = random.random()*5
+print(random_custom)
+
+
+
