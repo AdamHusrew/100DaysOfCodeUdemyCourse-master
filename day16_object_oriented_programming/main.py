8cd4e2c1efb420a0bad7b15f2163f79fdc21b7ee
AdamHusrew <111573037+AdamHusrew@users.noreply.github.com>
2022-10-22 14:06:44 +0300

    Day 16 Processing

---
+++ day16_object_oriented_programming/main.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.setx(23)
+timmy.sety(56)
+timmy.setheading(90)
+timmy.forward(200)
+
+
+my_screen = Screen()
+my_screen.exitonclick()
---
+++ day16_object_oriented_programming/pretty_table.py
@@ -0,0 +1,23 @@
+import os
+
+from prettytable import *
+
+table = PrettyTable()
+table.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
+table.add_row(["Adelaide", 1295, 1158259, 600.5])
+table.add_row(["Brisbane", 5905, 1857594, 1146.4])
+table.add_row(["Darwin", 112, 120900, 1714.7])
+table.add_row(["Hobart", 1357, 205556, 619.5])
+table.add_row(["Sydney", 2058, 4336374, 1214.8])
+table.add_row(["Melbourne", 1566, 3806092, 646.9])
+table.add_row(["Perth", 5386, 1554769, 869.4])
+
+print(table)
+print(table.bottom_junction_char * 51)
+
+table1 = PrettyTable()
+table1.add_column("City name", ["İstanbul", "Ankara", "İzmir", "Bursa"])
+table1.add_column("Population",[20_000_000,10_000_000,5_000_000,3_000_000])
+
+
+print(table1)
