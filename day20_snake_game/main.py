1de76101197b5c924afa91b3c2fc38d39f3f118c
Adam Husrew <Adam.Husrew@cydeo.com>
2022-12-27 16:49:09 +0300

    day 21 snake game done

--- day20_snake_game/main.py
+++ day20_snake_game/main.py
@@ -1,6 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from day21_snake_game_continue_and_inheritance.food import Food
+from day21_snake_game_continue_and_inheritance.score_board import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("Snake")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score_board = Scoreboard()
 screen.listen()
 
 screen.onkey(snake.up, "Up")
@@ -21,5 +25,24 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    #   Detect Collision with dot
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score_board.increase_score()
+        snake.extend()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score_board.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments:
+        if snake.head == segment:
+            pass
+        elif snake.head.distance(segment) < 5:
+            game_is_on = False
+            score_board.game_over()
 
 screen.exitonclick()
--- day20_snake_game/snake.py
+++ day20_snake_game/snake.py
@@ -14,15 +14,11 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+        self.head.shape("circle")
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.speed(1)
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -46,3 +42,14 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.speed(1)
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
